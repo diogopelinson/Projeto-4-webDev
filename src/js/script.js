3b83168dbd109f46e6b4a578153d26b6aa11fa05
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-05-06 09:52:32 -0300

    Adicionando o Quiz

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,20 @@ let imagens = [
     'src/assets/imagem2.jpg',
     'src/assets/imagem3.jpg',
 ];
+
+//DECLARANDO AS VARIAVEIS
+
+let i=0;
+let tempo = 3000;
+
+//CRIANDO A FUNÇÃO SLIDESHOW
+
+function slideShow(){
+    document.getElementById('image').src=imagens[i];
+    i++;
+    if( i == imagens.length){
+        i=0;
+    }
+    setTimeout("slideShow()", tempo)
+}
+slideShow();
