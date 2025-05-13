1db22e0c22b53aa194563f3224b67fae02db46a4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-13 09:42:48 -0300

    Criando GeoLocalização

---
+++ src/js/localiza.js
@@ -0,0 +1,27 @@
+//DECLARANDO A VARIAVEL
+let map = L.map('mapaid').setView([-23.5505, -46.6333],10);
+
+
+//DECLARAÇÂO A APLICAÇÃO DA API
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+//EVENTO PARA CHAMAR A FUNÇÃO MAPA
+map.on('click',mapa);
+
+
+//CRIANDO A FUNÇÃO MAPA
+function mapa(e){
+    let lat = e.latlng.lat.toFixed(6);
+    let long = e.latlng.lng.toFixed(6);
+    const locationInfo = document.getElementById('localizacao');
+
+    if(locationInfo){
+        locationInfo.textContent = `Localização escolhida: Latitude:${lat}, Longitude:${long}`;
+    }
+    L.marker(e.latlng).addTo(map)
+        .bindPopup(`Latitude:${lat}<br>Longitude:${long}`).openPopup();
+}
+map.on('click',mapa);
